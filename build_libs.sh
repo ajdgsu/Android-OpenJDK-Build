98002ef383cf4723c7fff099e936b7de1662c801
ajdgsu <45306865+ajdgsu@users.noreply.github.com>
2025-03-11 01:28:36 +0800

    Update build_libs.sh

--- build_libs.sh
+++ build_libs.sh
@@ -20,5 +20,5 @@ if [[ "$error_code" -ne 0 ]]; then
   exit $error_code
 fi
 
-CFLAGS="-fno-rtti -march=armv8.5-a" CXXFLAGS="-fno-rtti -march=armv8.5-a" make -j4
+CFLAGS="-fno-rtti -march=armv8.5-a -pipe" CXXFLAGS="-fno-rtti -march=armv8.5-a -pipe" make -j4
 make install
