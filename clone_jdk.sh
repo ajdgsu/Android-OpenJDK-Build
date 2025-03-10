09247490ffbd5a2bc7dcb524bd90a47e5be45609
ajdgsu <45306865+ajdgsu@users.noreply.github.com>
2025-03-11 01:20:34 +0800

    Update clone_jdk.sh

--- clone_jdk.sh
+++ clone_jdk.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
-git clone --branch jdk21.0.1 --depth 1 https://github.com/openjdk/jdk21u openjdk
+#git clone --branch jdk21.0.1 --depth 1 https://github.com/openjdk/jdk21u openjdk
+git clone --branch master --depth 1 https://github.com/openjdk/jdk21u openjdk
